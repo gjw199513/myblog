2fc8174b90253f0028c0d3de18e2505dd6d6030b
gjw199513@163.com <gjw605134015>
2018-11-24 11:31:13 +0800

    博客系统初始化

---
+++ src/main/java/com/gjw/blog/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.gjw.blog.repository;
+
+import com.gjw.blog.domain.User;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * 用户仓库.
+ *
+ * @since 1.0.0 2017年3月2日
+ * @author <a href="https://waylau.com">Way Lau</a> 
+ */
+public interface UserRepository extends CrudRepository<User, Long>{
+}
